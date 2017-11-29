8c8e235add3fdc57f96042c0a0492989deae6903
Ben <nitroccc@yahoo.com>
2017-11-29 14:06:45 -0600

    Cleaned up comments

--- src/assets/js/flight.js
+++ src/assets/js/flight.js
@@ -9,8 +9,8 @@ var terrainProvider = viewer.terrainProvider = new Cesium.CesiumTerrainProvider(
 function createModel(url, p, o) { // A fuction that allows you to create a plane with model from url, position from p, and orientation from o
     var entity = viewer.entities.add({ // Adding entity to Cesium Viewer
         name : url, // A human readable name to display to users
-        position : p, // A Property specifying the entity position
-        orientation : o, // A Property specifying the entity orientation
+        position : p,
+        orientation : o,
         model : { // A model to associate with this entity
             uri : url, // A string Property specifying the URI of the glTF asset
             scale : 0.4, // A uniform scale applied to this model
@@ -18,44 +18,44 @@ function createModel(url, p, o) { // A fuction that allows you to create a plane
         }
     });
     viewer.trackedEntity = entity; // Camera tracking the created entity
-    return entity; // Returns the created entity
+    return entity;
 }
 
 function createPath(p) { // A function that allows you to create a path with position from p
 	var ePath = viewer.entities.add({ // Adding entity to Cesium Viewer
-	  position : p, // A Property specifying the entity position
+	  position : p,
 	  path : { // A path to associate with this entity
 		  	material : new Cesium.PolylineOutlineMaterialProperty({ // A Property specifying the material used to draw the path
-	            color : Cesium.Color.WHITE, // A Property specifying the color of the line
-	            outlineWidth : 2, // A numeric Property specifying the width of the outline, in pixels
-	            outlineColor : Cesium.Color.WHITE.withAlpha(.5) // A property specifying the color of the outline
+	            color : Cesium.Color.WHITE, // Color of the line
+	            outlineWidth : 2, // Width in pixels
+	            outlineColor : Cesium.Color.WHITE.withAlpha(.5) // Color of the Outline
 	        }),
 			width : 3, // A numeric Property specifying the width in pixels
 			leadTime : 300, // A Property specifying the number of seconds in front the object to show
 			trailTime : 5 // A Property specifying the number of seconds behind of the object to show
 		}
 	})
-	return ePath; // Returns the created path
+	return ePath;
 }
 
 function createExceedancePath(p,c) { // A function that allows you to create an path for exceedances with position p and color c
 	var ePath = viewer.entities.add({
-	  position : p, // A Property specifying the entity position
+	  position : p,
 	  path : { // A path to associate with this entity
-		  	material : new Cesium.PolylineOutlineMaterialProperty({ // A Property specifying the material used to draw the path
-	            color : c, // A Property specifying the color of the line
-	            outlineWidth : 2, // A numeric Property specifying the width of the outline, in pixels
-	            outlineColor : c.withAlpha(.5) // A property specifying the color of the outline
+		  	material : new Cesium.PolylineOutlineMaterialProperty({ // Specifying the material used to draw the path
+	            color : c,
+	            outlineWidth : 2, // Outline width in pixels
+	            outlineColor : c.withAlpha(.5) // Color of the outline
 	        }),
-			width : 10, // A numeric Property specifying the width in pixels
-			leadTime : 300, // A Property specifying the number of seconds in front the object to show
-			trailTime : 5 // A Property specifying the number of seconds behind of the object to show
+			width : 10, // Width in pixels
+			leadTime : 300, // Number of seconds ahead to draw the poth
+			trailTime : 5 // Specifying the number of seconds behind the object to show
 		}
 	})
-	return ePath; // Retunts the exceedance path
+	return ePath;
 }
 
-// Defining variables
+// Necessary variables
 var time = [];
 var altitude = [];
 var ias = [];
@@ -88,14 +88,15 @@ orientationProperty.setInterpolationOptions({
 Cesium.loadText('./assets/data/testData.csv').then(function(text) { // Loading data from testData.csv
 	data = text.split(','); // Splitting testData by ','
 	var j = 0;
-	for(var i = 410; i < data.length - 1; i+=41) { // Iterates through the data
-		altitude[j] = parseFloat(data[i+5]); // Gets altitude from the data
-		ias[j] = parseFloat(data[i+6]); // Gets indicated air speed from the data
-		heading[j] = parseFloat(data[i+9]); // Gets heading from the data
-		pitch[j] = -parseFloat(data[i+11]); // Gets pitch from the data
-		roll[j] = -parseFloat(data[i+12]); // Gets roll from the data
-		latitude[j] = parseFloat(data[i+19]); // Gets latitude from the data
-		longitude[j] = parseFloat(data[i+20]); // Gets longitude from the data
+	//Parsing through the data and grabbing necessary information
+	for(var i = 410; i < data.length - 1; i+=41) {
+		altitude[j] = parseFloat(data[i+5]);
+		ias[j] = parseFloat(data[i+6]); //  indicated air speed
+		heading[j] = parseFloat(data[i+9]);
+		pitch[j] = -parseFloat(data[i+11]);
+		roll[j] = -parseFloat(data[i+12]);
+		latitude[j] = parseFloat(data[i+19]);
+		longitude[j] = parseFloat(data[i+20]);
 		position[j] = new Cesium.Cartesian3.fromDegrees(longitude[j], latitude[j], altitude[j] - 611); // Defines position in Cartesian3 (in degrees) coordinates from longitude, latitude, and position (subtracting 611 from position because of height from sealevel and model height)
 		hpr[j] = new Cesium.HeadingPitchRoll.fromDegrees(heading[j] + 90, pitch[j], roll[j]); // Defines hpr in a HeadingPitchRoll (in degrees) with heading (adding 90 to heading because model was 90 degrees off), pitch, and roll
 		orientation[j] = new Cesium.Transforms.headingPitchRollQuaternion(position[j], hpr[j]); // Defines orienation in a Quaternion tranformation with position and hpr
